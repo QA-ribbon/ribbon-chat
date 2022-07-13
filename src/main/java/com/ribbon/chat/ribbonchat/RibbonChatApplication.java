76a5a0ada957261b9079f118cd295ccbefe71c89
Jayant Kandpal <jayantkandpal@Jayants-MacBook-Air.local>
2022-07-13 13:44:46 +0530

    initial commit

---
+++ src/main/java/com/ribbon/chat/ribbonchat/RibbonChatApplication.java
@@ -0,0 +1,13 @@
+package com.ribbon.chat.ribbonchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ribbon/chat/ribbonchat/RibbonChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ribbon.chat.ribbonchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
